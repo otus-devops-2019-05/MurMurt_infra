aa6aba0fdf50661caa0b70e0ce46d9bc6b1e3e22
Ilnur Gataullin <i-gataullin@yandex-team.ru>
2019-09-23 05:55:32 +0300

    Ansible-3: Add ansible lint test

--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Custom tests";
-for file in $(find ./tests -depth 1 -name 'test_*');
+for file in $(find ./tests -name 'test_*');
     do
         echo "Run $file";
-#        docker exec -e USER=appuser hw-test ${file};
+        docker exec -e USER=appuser hw-test ${file};
     done
